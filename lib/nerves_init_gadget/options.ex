6e55925831b05f30e4c86d0f5d27cf22f16e0ffd
Connor Rigby <connor@farmbot.io>
2017-10-27 17:03:02 -0700

    allow for dhcp server instead of link local.

--- lib/nerves_init_gadget/options.ex
+++ lib/nerves_init_gadget/options.ex
@@ -2,7 +2,9 @@ defmodule Nerves.InitGadget.Options do
   @moduledoc false
 
   defstruct ifname: "usb0",
-            address_method: :static,
+            address_method: :linklocal,
+            ip_address: nil,
+            ip_subnet_mask: nil,
             mdns_domain: "nerves.local",
             node_name: nil,
             node_host: :mdns_domain
