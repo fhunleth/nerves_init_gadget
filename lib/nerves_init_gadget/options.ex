9d09e48a03267c88633806d57950c735034a0104
connor rigby <connor@farmbot.io>
2018-04-25 12:14:28 -0700

    Remove unused option

--- lib/nerves_init_gadget/options.ex
+++ lib/nerves_init_gadget/options.ex
@@ -6,6 +6,5 @@ defmodule Nerves.InitGadget.Options do
             mdns_domain: "nerves.local",
             node_name: nil,
             node_host: :mdns_domain,
-            ssh_console: false,
             ssh_console_port: 22
 end
